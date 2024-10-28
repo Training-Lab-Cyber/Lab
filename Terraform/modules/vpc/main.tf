a87e5dc57b5c675d4952f3628ddedb04d1808172
Lab <training.yound.lab@gmail.com>
2024-10-28 16:28:00 +0900

    ad

--- Terraform/modules/vpc/main.tf
+++ Terraform/modules/vpc/main.tf
@@ -49,24 +49,3 @@ resource "google_compute_firewall" "firewall_rules" {
   priority    = each.value.priority
 }
 
-
-resource "google_compute_router" "router" {
-  project = var.project
-  name    = "nat-router"
-  network = google_compute_network.vpc.id
-  region  = "us-west1"
-}
-
-resource "google_compute_router_nat" "nat" {
-  name   = "nat-config"
-  router = google_compute_router.router.name
-  region = google_compute_router.router.region
-
-  nat_ip_allocate_option             = "AUTO_ONLY"
-  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
-
-  log_config {
-    enable = true
-    filter = "ERRORS_ONLY"
-  }
-}
