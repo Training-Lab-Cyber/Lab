9f77d6ef649264033897b7abc43c68692cc8f69c
Lab <training.yound.lab@gmail.com>
2024-11-08 09:19:44 +0900

    ad

--- Terraform/modules/vpc/main.tf
+++ Terraform/modules/vpc/main.tf
@@ -15,6 +15,9 @@
 
 
 resource "google_compute_network" "vpc" {
+  name                    = var.vpc_name
+  project                 = var.project
+  auto_create_subnetworks = false
 }
 
 resource "google_compute_subnetwork" "subnet" {
